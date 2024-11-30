2612cbe07363b42581dbb70a934273554f9f1608
Harsha <harsha.pakanathi@gmail.com>
2024-11-30 17:12:58 +0530

    shell script

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,10 +1,12 @@
 
 resource "aws_instance" "terraform" {
+    count = 3
+
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
-        Name = "terraform"
+        Name = var.instance_names[count.index]
     }
 }
 
